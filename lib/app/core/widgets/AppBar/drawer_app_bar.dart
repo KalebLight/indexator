6fa9a92984fba7d6e4a0b2df010300d9a64f113c
KalebLight <kaleb.luiz00@gmail.com>
2024-08-21 08:46:29 -0300

    Create drawer_app_bar.dart

---
+++ lib/app/core/widgets/AppBar/drawer_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerAppBar extends StatelessWidget {
+  const DrawerAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
