75ce24cafe5970f5de03a4adffbc218c82e17c08
KalebLight <kaleb.luiz00@gmail.com>
2024-09-11 13:35:32 -0300

    Update user_model.dart

--- lib/app/modules/auth/models/user_model.dart
+++ lib/app/modules/auth/models/user_model.dart
@@ -17,5 +17,44 @@ class UserModel {
 class User {
   final String? token;
   final String? name;
-  User({this.token, this.name});
+  final String? email;
+  final String? id;
+  final String? googleId;
+  User({
+    this.token,
+    this.name,
+    this.email,
+    this.id,
+    this.googleId,
+  });
+
+  User copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? token,
+    String? googleId,
+  }) =>
+      User(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        token: token ?? this.token,
+        googleId: googleId ?? this.googleId,
+      );
+
+  factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        token: json["token"],
+        googleId: json["google_id"],
+      );
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "token": token,
+        "googleId": googleId,
+      };
 }
