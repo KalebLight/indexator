39d25323c49c380d7554d6cb4e2fb458189ec559
KalebLight <kaleb.luiz00@gmail.com>
2024-09-11 14:27:10 -0300

    create get user data

--- lib/app/core/store/user_store.dart
+++ lib/app/core/store/user_store.dart
@@ -1,3 +1,5 @@
+import 'package:indexator/app/core/data/status.dart';
+import 'package:indexator/app/core/repositories/user_repository.dart';
 import 'package:indexator/app/modules/auth/models/user_model.dart';
 import 'package:mobx/mobx.dart';
 
@@ -6,6 +8,29 @@ part 'user_store.g.dart';
 class UserStore = _UserStoreBase with _$UserStore;
 
 abstract class _UserStoreBase with Store {
+  final UserRepository userRepository;
+  _UserStoreBase(this.userRepository) {
+    if (user == null) getUserData();
+  }
+
   @observable
   User? user;
+
+  @observable
+  StatusDefault state = StatusIdle();
+
+  @action
+  getUserData() async {
+    state = StatusLoading();
+    var res = await userRepository.getUserData();
+    res.fold(
+      (l) {
+        state = StatusError();
+      },
+      (r) {
+        user = r;
+        state = StatusSuccess();
+      },
+    );
+  }
 }
