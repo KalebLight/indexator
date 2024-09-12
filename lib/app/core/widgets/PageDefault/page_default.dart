26c843e95bd69d6a7b3a637dad9bb98632b30577
KalebLight <kaleb.luiz00@gmail.com>
2024-09-12 13:48:55 -0300

    Create page_default.dart

---
+++ lib/app/core/widgets/PageDefault/page_default.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:indexator/app/core/data/breakpoints.dart';
+import 'package:indexator/app/core/widgets/AppBar/drawer_mobile_app_bar.dart';
+import 'package:indexator/app/core/widgets/AppBar/drawer_web_app_bar.dart';
+import 'package:indexator/app/core/widgets/AppBar/mobile_app_bar.dart';
+import 'package:indexator/app/core/widgets/AppBar/web_app_bar.dart';
+
+class PageDefault extends StatelessWidget {
+  final Widget body;
+  const PageDefault({super.key, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        appBar: constraints.maxWidth < mobileBreakpoint
+            ? const PreferredSize(preferredSize: Size(double.infinity, 52), child: MobileAppBar())
+            : const PreferredSize(preferredSize: Size(double.infinity, 72), child: WebAppBar()),
+        drawer: constraints.maxWidth < mobileBreakpoint ? const DrawerMobileAppBar() : const DrawerWebAppBar(),
+        body: body,
+      );
+    });
+  }
+}
