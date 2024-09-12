538d3402cf2a633bb206a4bc3872cfd3baa26b97
KalebLight <kaleb.luiz00@gmail.com>
2024-09-12 13:49:06 -0300

    use page default

--- lib/app/modules/home/pages/home_page.dart
+++ lib/app/modules/home/pages/home_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:indexator/app/core/data/breakpoints.dart';
-import 'package:indexator/app/core/widgets/AppBar/drawer_mobile_app_bar.dart';
-import 'package:indexator/app/core/widgets/AppBar/drawer_web_app_bar.dart';
-import 'package:indexator/app/core/widgets/AppBar/mobile_app_bar.dart';
-import 'package:indexator/app/core/widgets/AppBar/web_app_bar.dart';
+import 'package:indexator/app/core/widgets/PageDefault/page_default.dart';
 import 'package:indexator/app/modules/home/controllers/home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -31,11 +27,7 @@ class _HomePageState extends State<HomePage> {
       }
       return LayoutBuilder(
         builder: (context, constraints) {
-          return Scaffold(
-            appBar: constraints.maxWidth < mobileBreakpoint
-                ? const PreferredSize(preferredSize: Size(double.infinity, 52), child: MobileAppBar())
-                : const PreferredSize(preferredSize: Size(double.infinity, 72), child: WebAppBar()),
-            drawer: constraints.maxWidth < mobileBreakpoint ? const DrawerMobileAppBar() : const DrawerWebAppBar(),
+          return PageDefault(
             body: Center(
               child: Column(
                 children: [
--- lib/app/modules/urls/pages/urls_page.dart
+++ lib/app/modules/urls/pages/urls_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:indexator/app/core/data/breakpoints.dart';
 import 'package:indexator/app/core/data/colors_data.dart';
 import 'package:indexator/app/core/data/font_data.dart';
-import 'package:indexator/app/modules/urls/widgets/new_url_popUp.dart';
+import 'package:indexator/app/core/widgets/PageDefault/page_default.dart';
+import 'package:indexator/app/modules/urls/widgets/new_url_pop_up.dart';
 
 class URLsPage extends StatefulWidget {
   const URLsPage({super.key});
@@ -16,46 +17,44 @@ class _URLsPageState extends State<URLsPage> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return Scaffold(
-          backgroundColor: ColorsData.gunmetal,
-          body: ListView(
-            children: [
-              const SizedBox(height: 100),
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  width: constraints.maxWidth < mobileBreakpoint ? 400 : 700,
-                  height: 300,
-                  decoration: const BoxDecoration(
-                    color: ColorsData.timberwolf,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          showDialog(
-                              barrierDismissible: true,
-                              context: context,
-                              builder: (context) {
-                                return NewUrlPopup();
-                              });
-                        },
-                        child: Text(
-                          '+ Adicionar URL',
-                          style: FontData.bodyEmphasis1(ColorsData.success),
-                        ),
-                      )
-                    ],
+        return PageDefault(
+            body: ListView(
+          children: [
+            const SizedBox(height: 100),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                width: constraints.maxWidth < mobileBreakpoint ? 400 : 700,
+                height: 300,
+                decoration: const BoxDecoration(
+                  color: ColorsData.timberwolf,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
                   ),
                 ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        showDialog(
+                            barrierDismissible: true,
+                            context: context,
+                            builder: (context) {
+                              return NewUrlPopup();
+                            });
+                      },
+                      child: Text(
+                        '+ Adicionar URL',
+                        style: FontData.bodyEmphasis1(ColorsData.success),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ],
-          ),
-        );
+            ),
+          ],
+        ));
       },
     );
   }
