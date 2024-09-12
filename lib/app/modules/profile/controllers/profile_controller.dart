cb212e83e93124c4cd826d871d4af963138a6079
KalebLight <kaleb.luiz00@gmail.com>
2024-09-12 16:33:47 -0300

    add google logout

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,10 +1,18 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:indexator/app/core/store/user_store.dart';
+import 'package:indexator/app/modules/auth/apis/google_sign_in_api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileController {
+  final UserStore userStore;
+  ProfileController(this.userStore);
+
   Future<bool> logout() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.clear();
+    if (userStore.user!.googleToken != null) {
+      await GoogleSignInApi.logout();
+    }
     Modular.to.navigate('/auth/');
     return true;
   }
